4c3bb514aebc413b926410b04cb08c31051169cd
user1 <user1@DESKTOP-EK1IRA0>
2025-03-04 17:01:22 +0900

    3차 업로드

--- src/work/Ex7_homework.java
+++ src/work/Ex7_homework.java
@@ -1,7 +1,37 @@
 package work;
 
+import java.util.Scanner;
+
 public class Ex7_homework {
 	public static void main(String[] args) {
+		// single for문 사용
+		// 피보나치 수열 만들기
+		// 1  +  1   2   3  5  8  13  21 .... 
+		
+		// 반복횟수 : 6  -> 1 1 2 3 5 8 까지 출력
+		
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("반복횟수 : ");
+		int num = sc.nextInt();
+		
+		int res = 0;
+		int sum = 1;		
+		
+		for(int i = 1; i <= num; i++) {
+			
+			res = sum += i;  // sum = 1
+			System.out.print(sum + " ");
+			
+					
+			
+			
+			
+		} //for
+		
+		
+		
 		
 		
 		
---
+++ src/work/Ex8_homework.java
@@ -0,0 +1,39 @@
+package work;
+
+import java.util.Scanner;
+
+public class Ex8_homework {
+	public static void main(String[] args) {
+
+		// if문 사용
+
+		// 키보드에서 연도를 입력받고, 해당 연도가 윤년인지 판단.
+
+		// 조건1 : 4로 나누어 떨어지지만, 100으로는 나누어지지 않는 연도
+		// 조건2 : 400으로 나누어 떨어지는 연도
+
+		// 연도 : 200
+		// 2000년은 윤년입니다.
+
+		// 연도 : 2025
+		// 2025년은 평년입니다.
+
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("연도 : ");
+		int year = sc.nextInt();
+
+		if( year % 400 == 0 ) {
+			if(year % 4 == 0  && year % 100 == 0) {
+				System.out.println(year + "년은 윤년입니다.");
+			}
+		
+		} else {
+			System.out.println(year + "년은 평년입니다.");
+		}
+
+
+     } // main
+
+}
